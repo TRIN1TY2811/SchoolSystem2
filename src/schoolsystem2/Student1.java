aba342da9e6e6d338ec343b5d4a1932b93a2cb14
TRIN1TY2811 <jamessearll28@gmail.com>
2026-04-02 16:44:44 +0800

    Personal Information

--- src/schoolsystem2/Student1.java
+++ src/schoolsystem2/Student1.java
@@ -9,7 +9,8 @@ import java.awt.*;
  */
 public class Student1 extends JFrame {
 
-    private JLabel lblName, lblTitle;
+    private JLabel lblTitle,lblNameTitle,lblAgeTitle,lblYearTitle,lblCourseTitle;
+    private JTextField txtName,txtAge,txtYear,txtCourse;
 
     Student1() {
 
@@ -18,8 +19,48 @@ public class Student1 extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         lblTitle = new JLabel("Personal Information");
-        lblTitle.setBounds(250, 50, 400, 50);
+        lblTitle.setBounds(300, 50, 400, 50);
         lblTitle.setFont(new Font("Arial", Font.PLAIN, 24));
         this.add(lblTitle);
+        
+        txtName = new JTextField ("Amodia, James Earl");
+        txtName.setBounds (200, 150, 400,50);
+        txtName.setHorizontalAlignment(JTextField.CENTER);
+        txtName.setEditable(false);
+        add(txtName);
+        
+        lblNameTitle = new JLabel("Name");
+        lblNameTitle.setBounds(380,200,400,50);
+        add(lblNameTitle);
+        
+        txtAge = new JTextField ("20");
+        txtAge.setBounds (200, 250, 400,50);
+        txtAge.setHorizontalAlignment(JTextField.CENTER);
+        txtAge.setEditable(false);
+        add(txtAge);
+        
+        lblAgeTitle = new JLabel("Age");
+        lblAgeTitle.setBounds(380,300,400,50);
+        add(lblAgeTitle);
+        
+        txtYear = new JTextField ("Second Year");
+        txtYear.setBounds (200, 350, 400,50);
+        txtYear.setHorizontalAlignment(JTextField.CENTER);
+        txtYear.setEditable(false);
+        add(txtYear);
+        
+        lblYearTitle = new JLabel("Year");
+        lblYearTitle.setBounds(380,400,400,50);
+        add(lblYearTitle);
+        
+        txtCourse = new JTextField ("Information Technology");
+        txtCourse.setBounds (200, 450, 400,50);
+        txtCourse.setHorizontalAlignment(JTextField.CENTER);
+        txtCourse.setEditable(false);
+        add(txtCourse);
+        
+        lblCourseTitle = new JLabel("Course");
+        lblCourseTitle.setBounds(380,500,400,50);
+        add(lblCourseTitle);
     }
 }
