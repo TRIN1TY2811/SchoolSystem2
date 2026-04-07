26426a81ac92285272d4de38be2d476c1da0c50a
jouhakie-boop <jouhakie@gmail.com>
2026-04-07 19:19:38 +0800

    inayos

--- src/StudentInput/PersonalInfo.java
+++ src/StudentInput/PersonalInfo.java
@@ -4,9 +4,11 @@
  */
 package StudentInput;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.*;
 
 /**
  *
@@ -14,11 +16,16 @@ import javax.swing.JFrame;
  */
 public class PersonalInfo extends JFrame implements ActionListener{
 
-    
+    JLabel lblPersonalInfo;
   public PersonalInfo(){
     setSize(800, 750); 
     setLayout(null);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    
+    lblPersonalInfo = new JLabel("Student Record System: ");
+    lblPersonalInfo.setBounds(200, 50, 500, 50);
+    lblPersonalInfo.setFont(new Font("Arial", Font.PLAIN,24));
+    this.add(lblPersonalInfo);
     
   }
     @Override
